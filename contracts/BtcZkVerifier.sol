9bc5f5e7ed4ae61b3a92f0526c3e2821a33584a3
bl2longer <154850898+bl2longer@users.noreply.github.com>
2024-04-04 01:24:12 +0800

    Update BtcZkVerifier.sol
    adjust code

--- contracts/BtcZkVerifier.sol
+++ contracts/BtcZkVerifier.sol
@@ -3,78 +3,117 @@ pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 
-import "./interfaces/IBtcZkVerifier.sol";
+import "./interfaces/IBtcTxVerifier.sol";
 import "./interfaces/IBtcBlockHeader.sol";
-import "./interfaces/IZkpOrder.sol";
-import { MerkleProof} from "./lib/MerkleProof.sol";
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+import "./interfaces/IBtcTxZkp.sol";
+import { MerkleProof } from "./lib/MerkleProof.sol";
 
-contract BtcZkVerifier is OwnableUpgradeable, IBtcZkVerifier {
+contract BtcTxVerifier is OwnableUpgradeable, IBtcTxVerifier {
+    address public btcTxZkpAddr;
+    address public btcHeaderAddr;
 
-    address public zkpOrder;
-    address public btcHeaderData;
-
-    mapping(bytes32 => VerifierData) private verifyRecords;
+    mapping(bytes32 => TxVerifyRecord) private txVerifyRecords;
 
     ///@custom:oz-upgrades-unsafe-allow constructor
-    constructor() public { _disableInitializers(); }
+    constructor(address _btcTxZkpAddr, address _btcHeaderAddr) {
+        btcTxZkpAddr = _btcTxZkpAddr;
+        btcHeaderAddr = _btcHeaderAddr;
+    }
 
-    function initialize() initializer public  {
+    /// @dev Initialize the contract
+    function initialize() public initializer {
         __Ownable_init(msg.sender);
-        zkpOrder = 0x2cD6A5A6e716b1dEF6587afbb085303721fe8201;
-        btcHeaderData = 0x7a581772B0b21f5B8880E881C495cb7AfDfA228c;
     }
 
-    function addBtcTx(bytes memory btcTxRawData,
+    /// @dev Submit a Bitcoin transaction for verification and ZKP generation
+    /// @param rawTx The raw data of the Bitcoin transaction
+    /// @param utxos The UTXOs of the transaction
+    /// @param blockHeight The block height of the transaction
+    /// @param merkleProof The Merkle proof of the transaction
+    /// @param blockMerkleRoot The Merkle root of the block
+    /// @param txHash The hash of the Bitcoin transaction
+    /// @param proofPositions The positions of the transaction in the Merkle tree
+    function verifyBtcTx(
+        bytes memory rawTx,
         bytes[] memory utxos,
-        uint32 inBtcHeight,
-        bytes32[] memory proofPath,
-        bytes32 merkleRoot,
-        bytes32 btcTx,
-        bool[] memory positions) external {
-        BlockHeader memory header = IBtcBlockHeader(btcHeaderData).getBlockByHeight(inBtcHeight);
-        require(header.merkleRoot == merkleRoot, "errorMerkleRoot");
+        uint32 blockHeight,
+        bytes32[] memory merkleProof,
+        bytes32 blockMerkleRoot,
+        bytes32 txHash,
+        bool[] memory proofPositions
+    ) external {
+        require(rawTx.length > 0, "InvalidRawTx");
+        require(utxos.length > 0, "InvalidUtxos");
+        require(merkleProof.length > 0, "InvalidMerkleProof");
 
-        bytes32 root = MerkleProof.getBitCoinMerkleRoot(proofPath, btcTx, positions);
-        require(root == merkleRoot, "verifyMerkleFailed");
-        bytes32 txID = IZkpOrder(zkpOrder).addTransaction(btcTxRawData, utxos);
+        BlockHeader memory header = IBtcBlockHeader(btcHeaderAddr).getBlockByHeight(blockHeight);
+        require(header.merkleRoot == blockMerkleRoot, "InvalidBlockMerkleRoot");
 
-        verifyRecords[btcTx] = VerifierData(zkpOrder, txID);
-        txID = MerkleProof.reverseBytes32(txID);
-        require(txID == btcTx, "errorTx");
-        emit BtcTxAdded(btcTx);
+        bytes32 calculatedRoot = MerkleProof.getBitcoinMerkleRoot(merkleProof, txHash, proofPositions);
+        require(calculatedRoot == blockMerkleRoot, "InvalidMerkleProof");
+        
+        require(btcTxZkpAddr != address(0), "InvalidBtcTxZkpAddress");
+        bytes32 zkpID = IBtcTxZkp(btcTxZkpAddr).generateTxProof(rawTx, utxos);
+
+        txVerifyRecords[txHash] = TxVerifyRecord(btcTxZkpAddr, zkpID);
+        zkpID = MerkleProof.reverseBytes32(zkpID);
+        require(zkpID == txHash, "ZkpIDMismatch");
+        emit BtcTxVerified(txHash);
     }
 
-    function getBtcTxRecord(bytes32 btcTx) external view returns(VerifierData memory) {
-        return verifyRecords[btcTx];
+    /// @dev Get the verification record of a Bitcoin transaction
+    /// @param txHash The hash of the Bitcoin transaction
+    /// @return The verification record
+    function getTxVerifyRecord(bytes32 txHash) external view returns(TxVerifyRecord memory) {
+        return txVerifyRecords[txHash];
     }
 
-    function getBtcTxStatus(bytes32 btcTx) external view returns(ProofStatus) {
-        VerifierData memory record = verifyRecords[btcTx];
-        require(record.zkpOrder != address(0), "NoRecord");
-        return IZkpOrder(record.zkpOrder).getOrderStatus(record.zkpTxID);
+    /// @dev Get the status of a Bitcoin transaction ZKP
+    /// @param txHash The hash of the Bitcoin transaction
+    /// @return The status of the ZKP
+    function getTxZkpStatus(bytes32 txHash) external view returns(ProofStatus) {
+        TxVerifyRecord memory record = txVerifyRecords[txHash];
+        if (record.btcTxZkpAddr == address(0)) {
+            revert("RecordNotFound");
+        }
+        return IBtcTxZkp(record.btcTxZkpAddr).getProofStatus(record.zkpID);
     }
 
-    function getBtcTxDetails(bytes32 btcTx, string memory network) external view returns(string[] memory btcAddrs,
-                                                                                      uint256[] memory amounts,
-                                                                                      uint256 txfee) {
-        VerifierData memory record = verifyRecords[btcTx];
-        require(record.zkpOrder != address(0), "NoRecord");
-        return IZkpOrder(record.zkpOrder).getOrderDetails(record.zkpTxID, network);
+    /// @dev Get the details of a verified Bitcoin transaction
+    /// @param txHash The hash of the Bitcoin transaction
+    /// @param network The network of the transaction
+    /// @return btcAddresses The Bitcoin addresses of the transaction
+    /// @return amounts The amounts of the transaction
+    /// @return fee The transaction fee
+    function getVerifiedTxDetails(bytes32 txHash, string memory network) external view returns(
+        string[] memory btcAddresses,
+        uint256[] memory amounts,
+        uint256 fee
+    ) {
+        TxVerifyRecord memory record = txVerifyRecords[txHash];
+        if (record.btcTxZkpAddr == address(0)) {
+            revert("RecordNotFound");
+        }
+        return IBtcTxZkp(record.btcTxZkpAddr).getTxDetails(record.zkpID, network);
     }
 
-    function setBTCHeaderDataAddress(address btcHeader) external onlyOwner {
-        emit BTCHeaderDataChanged(btcHeaderData, btcHeader);
-        btcHeaderData = btcHeader;
+    /// @dev Set the address of the Bitcoin block header contract
+    /// @param newBtcHeaderAddr The address of the new Bitcoin block header contract
+    function setBtcHeaderAddr(address newBtcHeaderAddr) external onlyOwner {
+        emit BtcHeaderAddrChanged(btcHeaderAddr, newBtcHeaderAddr);
+        btcHeaderAddr = newBtcHeaderAddr;
     }
 
-    function setZkpOrder(address zkpOrderAddress) external onlyOwner {
-        emit ZKPOrderChanged(zkpOrder, zkpOrderAddress);
-        zkpOrder = zkpOrderAddress;
+    /// @dev Set the address of the Bitcoin transaction ZKP contract
+    /// @param newBtcTxZkpAddr The address of the new Bitcoin transaction ZKP contract 
+    function setBtcTxZkpAddr(address newBtcTxZkpAddr) external onlyOwner {
+        emit BtcTxZkpAddrChanged(btcTxZkpAddr, newBtcTxZkpAddr);
+        btcTxZkpAddr = newBtcTxZkpAddr;
     }
 
-    function lastBtcHeight() external view returns(uint256) {
-        return IBtcBlockHeader(btcHeaderData).lastHeight();
+    /// @dev Get the last synced Bitcoin block height
+    /// @return The last synced block height
+    function getLastBtcHeight() external view returns(uint256) {
+        return IBtcBlockHeader(btcHeaderAddr).lastHeight();
     }
 }
